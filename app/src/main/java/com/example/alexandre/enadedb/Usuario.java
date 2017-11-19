47a3511403e5e787f6c70af3879ce63ca9075f43
Alexandre.vsgouveia <alexandre.vsgouveia@gmail.com>
2017-11-19 00:27:35 -0300

    change class user to implements parcelable

--- app/src/main/java/com/example/alexandre/enadedb/Usuario.java
+++ app/src/main/java/com/example/alexandre/enadedb/Usuario.java
@@ -1,76 +1,66 @@
 package com.example.alexandre.enadedb;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Created by alexandre on 16/11/17.
  */
 
-public class Usuario {
-   private String name;
-   private String lastName;
-   private String email;
-   private String course;
-   private String instEnsino;
-   private ArrayList<Historico> historico;
+public class Usuario implements Parcelable {
+    private String name;
+    private String lastName;
+    private String email;
+    private String course;
+    private String instEnsino;
+    private ArrayList<Historico> historico;
 
-
-
-    public Usuario(String name, String lastName, String email, String course, String instEnsino) {
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-        this.course = course;
-        this.instEnsino = instEnsino;
-	historico = new ArrayList();
+    protected Usuario(Parcel in) {
+        name = in.readString();
+        lastName = in.readString();
+        email = in.readString();
+        course = in.readString();
+        instEnsino = in.readString();
+        if (in.readByte() == 0x01) {
+            historico = new ArrayList<Historico>();
+            in.readList(historico, Historico.class.getClassLoader());
+        } else {
+            historico = null;
+        }
     }
 
-
-    public String getCourse() {
-        return course;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public void setCourse(String course) {
-        this.course = course;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(lastName);
+        dest.writeString(email);
+        dest.writeString(course);
+        dest.writeString(instEnsino);
+        if (historico == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(historico);
+        }
     }
 
-    public String getName() {
-        return name;
-    }
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Usuario> CREATOR = new Parcelable.Creator<Usuario>() {
+        @Override
+        public Usuario createFromParcel(Parcel in) {
+            return new Usuario(in);
+        }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getInstEnsino() {
-        return instEnsino;
-    }
-
-    public void setInstEnsino(String instEnsino) {
-        this.instEnsino = instEnsino;
-    }
-
-    public ArrayList<Historico> getHistorico() {
-        return historico;
-    }
-
-    public void setHistorico(ArrayList<Historico> historico) {
-        this.historico = historico;
-    }
+        @Override
+        public Usuario[] newArray(int size) {
+            return new Usuario[size];
+        }
+    };
 }
